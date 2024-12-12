6b7cd537026391a3276fb378cfecabd1aad36ab0
GitHub Action <action@github.com>
2024-12-12 08:09:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/rusnyz/stopcoding/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/rusnyz/stopcoding/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.rusnyz.stopcoding.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.rusnyz.stopcoding.MyBundle
+import com.github.rusnyz.stopcoding.services.MyProjectService
 import javax.swing.JButton
 
 
